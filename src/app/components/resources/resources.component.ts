bcbfd85efd847867d5e5a541b39d64db0d72056b
chjuca <chjuca@utpl.edu.ec>
2021-06-03 22:09:13 -0500

    Add resources cards

--- src/app/components/resources/resources.component.ts
+++ src/app/components/resources/resources.component.ts
@@ -13,12 +13,34 @@ export class ResourcesComponent implements OnInit {
 
   resources = [];
   subscription: Subscription;
+  resourcesThree = [];
+  current = []
+  result = []
+
 
   constructor(public resourceService: ResourceService, public router: Router) { }
 
   ngOnInit() {
     this.resources = [];
-    this.subscription = this.resourceService.findAllResources().subscribe(resources => { this.resources = resources })
+    this.subscription = this.resourceService.findAllResources().subscribe(resources => {
+      resources.forEach(resource => {
+        if (this.current.length == 3) {
+          this.current.push(resource);
+          this.result.push(this.current);
+          this.current = []
+        } else {
+          this.current.push(resource);
+        }
+      });
+      if (this.current.length != 0) {
+        for (let i = this.current.length; i < 4; i++) {
+          this.current.push({});
+        }
+      }
+      this.result.push(this.current);
+      console.log(this.result);
+    })
+
   }
 
   showResource(resource: Resources) {
