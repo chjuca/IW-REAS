b6a54e126b2653991386675997805ff4935bfdbc
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2021-06-24 08:21:06 -0500

    Nav services

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Output } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/user.interface';
 import { AuthenticationService } from './../../services/authentication.service';
@@ -8,16 +8,16 @@ import { AuthenticationService } from './../../services/authentication.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   constructor(private authenticationService: AuthenticationService) { }
   ngOnInit(){
-    console.log(this.authenticationService.isLoggedIn);
-   /* if(localStorage.getItem('user')!==null){
+ 
+   /*if(localStorage.getItem('user')!==null){
       this.entry = true;
     }else{
       this.entry = false;
-    } */
+    }*/
   }
   email: string;
   password: string;
@@ -25,18 +25,18 @@ export class LoginComponent implements OnInit {
   mensaje: string;
   subscription: Subscription;
   user: User;
- 
+  ngOnDestroy(){
 
+  }
   async signIn() {
     
     await this.authenticationService.SignIn(this.email, this.password)
     if(this.authenticationService.isLoggedIn){
       this.entry = true;
 
-      this.authenticationService.getRolFromEmail(this.email);
       this.subscription = this.authenticationService.getRolFromEmail(this.email).subscribe(user => {
-        this.user = user;
-        console.log(this.user.rol)
+       this.user = user;
+        //console.log(this.user.rol)
       })
     }else{
       this.entry = false;
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { User } from '../models/user.interface';
 
 @Injectable({
@@ -13,20 +14,42 @@ import { User } from '../models/user.interface';
 export class AuthenticationService {
     isLoggedIn = false;
     mensaje = "";
+    rol = "";
     COLLECTION_NAME_USERS = 'users';
     userCollection: AngularFirestoreCollection;
+    subscription: Subscription;
     user: User
+    userInfo: User
     
 
     constructor(private angularFireAuth: AngularFireAuth, private db: AngularFirestore) {
        
     }
 
-
       getRolFromEmail(id: string) {
         this.userCollection = this.db.collection(this.COLLECTION_NAME_USERS);
+        this.subscription = this.userCollection.doc(id).valueChanges().subscribe(user => {
+            this.userInfo = user;
+            this.userInfo.email = id
+            console.log(this.userInfo.rol);
+          })
+        
         return this.userCollection.doc(id).valueChanges();
+
       }
+      
+      getRol(id: string) {
+        this.userCollection = this.db.collection(this.COLLECTION_NAME_USERS);
+        this.subscription = this.userCollection.doc(id).valueChanges().subscribe(user => {
+            this.userInfo = user;
+            this.userInfo.email = id
+            //console.log(this.userInfo.rol);
+          })
+        
+        return this.userInfo
+
+      }
+  
       
     addUser(user: User) {
         this.userCollection = this.db.collection(this.COLLECTION_NAME_USERS);
@@ -68,11 +91,9 @@ export class AuthenticationService {
 
 
     /* Sign out */
-    SignOut() {
-        this.angularFireAuth
-            .auth
-            .signOut();
-        localStorage.removeItem('user');
+    async SignOut() {
+      this.angularFireAuth.auth.signOut();
+      localStorage.removeItem('user');
     }
 
 }
